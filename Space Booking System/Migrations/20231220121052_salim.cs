9b5eab04ec5aa3481dccf1c1fea3e54be3947b09
salim <salimyaaqoubii@gmail.com>
2023-12-20 16:13:18 +0400

    edited

---
+++ Space Booking System/Migrations/20231220121052_salim.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Space_Booking_System.Migrations
+{
+    public partial class salim : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
